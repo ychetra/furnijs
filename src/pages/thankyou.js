d114d920c1edb8c77710bf39064416729ffb6b93
ychetra <chetra.storeit@gmail.com>
2024-12-02 11:06:11 +0700

    Thank you page

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -49,7 +49,7 @@ function Thankyou() {
               <h2 className="display-3 text-black">Thank you!</h2>
               <p className="lead mb-5">You order was successfuly completed.</p>
               <p>
-                <a href="shop.html" className="btn btn-sm btn-outline-black">
+                <a href="shop" className="btn btn-sm btn-outline-black">
                   Back to shop
                 </a>
               </p>
